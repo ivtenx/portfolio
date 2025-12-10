95a2d8e005e8b6337ff3365ca234902e49091380
Sabbir Rafi <SABBIRRAFI1000@GMAIL.COM>
2025-12-10 23:43:33 +0600

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@
 const Hero = () => {
 
     return (
-        <div className="border border-gray-400 rounded-lg mx-3 mt-8 flex justify-between 2xl:py-[1vw] 2xl:pl-[1vw] p-2">
+        <div className="border border-gray-400 rounded-lg mx-3 mt-8 flex justify-between 2xl:py-[1vw] 2xl:pl-[1vw] p-2 backdrop-blur-lg">
             <div>            <div><span className="text-gray-300 text-sm">Hey, Welcome!</span><div></div><span className="text-gray-300 text-2xl">Sabbir's Portfolio</span></div><div><span className="text-gray-300 text-sm">Aspiring FullStack Engineer+Aloof-ish</span></div>
             </div>
 

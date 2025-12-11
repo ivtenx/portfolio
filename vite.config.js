2ce1cc703c15e889c7734148f89ad99686522fa4
Sabbir Rafi <SABBIRRAFI1000@GMAIL.COM>
2025-12-11 14:50:23 +0600

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import prerender from 'vite-plugin-pretender'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), prerender({ staticDir: 'dist', routes: ['/', '/about'] })],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
 })

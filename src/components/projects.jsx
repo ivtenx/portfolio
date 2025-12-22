e570883775b81b9df4d5581f108a44e07c4da317
Sabbir Rafi <SABBIRRAFI1000@GMAIL.COM>
2025-12-22 10:32:09 +0600

    Update project description and demo link

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -31,8 +31,8 @@ const Projects = () => {
                     <div className="mx-2">
 
                         <span className="text-purple-200 text-xl font-cdi">CUETmem Game</span> <br />
-                        <span className="text-gray-400 text-md ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, aliquam quaerat odio exercitationem ipsam neque et repudiandae corporis, voluptatibus veritatis dolore fuga eaque totam, temporibus facere sunt illum ducimus fugiat?</span><br />
-                        <button className="text-gray-300 border font-mono px-0.5 bg-purple-800 border-gray-400 rounded-lg hover:scale-110 mt-8 mb-2.5 absolute bottom-0">Watch Demo</button>
+                        <span className="text-gray-400 text-md ">This made was dedicated to my uni-Friends, who keeps by my side all over my tough times. Just match pairs and Play ON!</span><br />
+                        <button className="text-gray-300 border font-mono px-0.5 bg-purple-800 border-gray-400 rounded-lg hover:scale-110 mt-8 mb-2.5 absolute bottom-0"><a href="cuetmem.netlify.app" target="_blank">Watch demo</a></button>
 
 
 
